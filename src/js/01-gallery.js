73030e4a03cbf31ee05774aa7868695d9a2336ac
Alex Koval <301019a@gmail.com>
2022-12-02 13:25:48 +0200

    some changes

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,39 @@
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+//____________________________________________________// Створюємо галерею //_______________________________//
+const galleryEl = document.querySelector('.gallery'); // шукаємо gallery
+
+const createGallery = galleryItems.reduce(
+  (acc, { preview, original, description }) => {      // перебираємо масив фото
+    let string =                                      // створюємо шаблонний рядок
+    `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" /></a>`;
+    return acc + string;                              // робимо один рядок html
+  },'');
+
+galleryEl.insertAdjacentHTML(                         // вставляємо масим фото
+  'beforeend',                                        // вибираємо місце розгортування
+  createGallery                                       // вибираємо об"єкт розгортування
+);
+
+//____________________________________________________// Клікаємо по зображенню //_______________________________//
+
+  new SimpleLightbox(".gallery a", {                  // Всі налаштування пропонує бібліотека
+    captionsData: "alt",
+    captionDelay: 250,
+    animationSpeed: 250,
+    overlay: true,
+    close: true,
+    showCounter: true,
+    fadeSpeed: 250,
+    loop: false,
+  });
+
 console.log(galleryItems);
+
+
